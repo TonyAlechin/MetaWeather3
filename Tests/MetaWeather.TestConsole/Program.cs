4a9114a4f64922e28d22acffda84281bbadf63d7
Anton Alechin <antonalechin2@gmail.com>
2025-01-21 23:40:35 +0300

    настройка хоста

--- Tests/MetaWeather.TestConsole/Program.cs
+++ Tests/MetaWeather.TestConsole/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MetaWeather.TestConsole
+{
+    class Program
+    {
+        private static IHost _Hosting;
+
+        public static IHost Hosting => _Hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(ConfigureServices);
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            
+        }
+
+        static async Task Main(string[] args)
+        {
+            using var host = Hosting;
+            await host.StartAsync();
+
+            Console.WriteLine("Завершено!");
+
+            Console.ReadLine();
+            await host.StopAsync();
+
+        }
+    }
+}
